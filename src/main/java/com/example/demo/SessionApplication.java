2ec8e850057a40331fa90653bf28ddb525c39c31
codegymdanang <codegymdanang@gmail.com>
2019-08-02 15:22:37 +0700

    build web

--- src/main/java/com/example/demo/SessionApplication.java
+++ src/main/java/com/example/demo/SessionApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SessionApplication {
+public class SessionApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         SpringApplication.run(SessionApplication.class, args);
+    } */
+   public static void main(String[] args) {
+       SpringApplication.run(SessionApplication.class, args);
+   }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SessionApplication.class);
     }
+
 
 }
